39d6e41d2163eed1205b2ead72b398b4c2a733f0
brendan <brglackin@gmail.com>
2014-09-19 21:17:06 -0700

    First pass project 1

---
+++ analysis.R
@@ -0,0 +1,9 @@
+
+load(url("http://bit.ly/dasi_gss_data"))
+gss<-gss[,c("year","polviews","natenvir")]
+gss<-gss[complete.cases(gss),]
+
+png("rough_draft.png")
+mosaicplot(polviews~natenvir,gss,main="Political Outlook versus Opinion on Environmental Spending",xlab="Political Outlook", ylab="Opinion on Environmental Spending")
+dev.off()
+
